994bfca95009fe4bae8ae8461c84f8ac81a7e681
cherry4xo <1jlaketuk1@gmail.com>
2023-10-13 23:59:32 +0300

    v0.0.1.3: fixed bug with expired token

--- backend/app/core/auth/utils/jwt.py
+++ backend/app/core/auth/utils/jwt.py
@@ -12,10 +12,11 @@ access_token_jwt_subject = "access"
 def create_access_token(*, data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.utcnow() + timedelta(hours=3) + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire, "sub": access_token_jwt_subject})
+        expire = datetime.utcnow() + timedelta(hours=3) + timedelta(minutes=15)
+    to_encode.update({"exp": expire.timestamp(), "sub": access_token_jwt_subject})
     encoded_jwt = jwt.encode(jsonable_encoder(to_encode), settings.SECRET_KEY, algorithm=ALGORITHM)
+    print(jsonable_encoder(to_encode))
     
     return encoded_jwt
