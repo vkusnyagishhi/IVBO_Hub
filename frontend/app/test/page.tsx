c341cc26b7ca37ca993be94e3c40d1a713a5691e
mkrkru <42581149+mkrkru@users.noreply.github.com>
2023-10-17 19:21:25 +0300

    calendar upd

--- frontend/app/test/page.tsx
+++ frontend/app/test/page.tsx
@@ -1,9 +1,10 @@
 'use client';
-import { HStack, Text, VStack } from "@chakra-ui/react";
+import { HStack, Text, VStack, Box } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useDispatch, useSelector } from "@/redux/hooks";
 import { setTable } from "@/redux/miscSlice";
+import { motion } from "framer-motion";
 
 export default function TestPage() {
     const dispatch = useDispatch();
@@ -15,38 +16,30 @@ export default function TestPage() {
     }, [dispatch]);
 
     return <VStack w='max-content'>
-        <HStack w={isLaptop ? '47%' : '75%'} color='gray.500' spacing='22px' justify={isLaptop ? 'end' : 'center'}>
+        <HStack ml='-47px' color='gray.500' spacing='22px' justify={isLaptop ? 'end' : 'center'}>
             {['неделя', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'].map(d => <Text key={d}>{d}</Text>)}
         </HStack>
 
         {data.length > 0 && data.map((week: object[], i) => <HStack key={i} color='white' spacing='10px'>
-            <Text w='20px' align='center'>{i + 1}</Text>
-
             {week.map((dayTable: any, j) => {
                 const isEmpty = dayTable.every((l: any) => !l);
-                let day = 7 * i + j + 1 - 4;
-                let month = 0;
+                let day = i === 0 && j !== 6 ? 0 : 7 * i + j - 5;
+                let month = 10;
 
-                if (day > 30 && day <= 61) {
-                    day -= 30;
-                    month = 9;
-                } else if (day > 61 && day <= 91) {
-                    day -= 61;
-                    month = 10;
-                } else if (day > 91 && day <= 121) {
-                    day -= 91;
+                if (day > 31) {
+                    day -= 31;
                     month = 11;
                 }
 
                 const cellColor = weekIndex === i && weekDayIndex === j
                     ? 'purple.500'
                     : new Date().getDate() === day && new Date().getMonth() === month
-                        ? 'green.500'
+                        ? 'green.400'
                         : isEmpty
-                            ? 'gray.500'
-                            : 'blue.600';
+                            ? 'gray.600'
+                            : 'blue.700';
 
-                return <VStack w='30px' color='white' key={j} bg={cellColor} _hover={{ cursor: 'pointer' }} onClick={() => {
+                return <VStack w='30px' color='white' key={j} bg={cellColor} _hover={{ cursor: 'pointer' }} transition='0.2s' onClick={() => {
                     if (!isEmpty) setSelected([i, j]);
                 }}>
                     {day > 0 && <Text>{day}</Text>}
@@ -54,19 +47,21 @@ export default function TestPage() {
             })}
         </HStack>)}
 
-        {data[weekIndex] && data[weekIndex][weekDayIndex] && Object.keys(data[weekIndex][weekDayIndex]).length > 0
-            ? <VStack>
-                {Object.keys(data[weekIndex][weekDayIndex]).map((lesson: string, i) => {
-                    const theLesson = data[weekIndex][weekDayIndex][lesson];
-                    if (!theLesson) return <Text color='white'>{i + 1}. [ОКНО]</Text>;
+        <motion.div style={{ height: '20vh' }} initial={{ y: 30, opacity: 0 }} animate={{ y: 0, opacity: 1 }} exit={{ y: -30, opacity: 0 }} transition={{ duration: 0.3 }} key={weekIndex + weekDayIndex}>
+            {data[weekIndex] && data[weekIndex][weekDayIndex] && Object.keys(data[weekIndex][weekDayIndex]).length > 0
+                ? <VStack h='100%' border='2px solid red' key={weekIndex + weekDayIndex}>
+                    {Object.keys(data[weekIndex][weekDayIndex]).map((lesson: string, i) => {
+                        const theLesson = data[weekIndex][weekDayIndex][lesson];
+                        if (!theLesson) return <Text color='white'>{i + 1}. [ОКНО]</Text>;
 
-                    const { PROPERTY_DISCIPLINE_NAME, PROPERTY_LESSON_TYPE, PROPERTY_LECTOR, PROPERTY_PLACE } = theLesson;
+                        const { PROPERTY_DISCIPLINE_NAME, PROPERTY_LESSON_TYPE, PROPERTY_LECTOR, PROPERTY_PLACE } = theLesson;
 
-                    return <HStack key={i} color='white'>
-                        <Text>{i + 1}. {PROPERTY_DISCIPLINE_NAME} {`//`} {PROPERTY_LESSON_TYPE} {`//`} {PROPERTY_LECTOR} {`//`} {PROPERTY_PLACE}</Text>
-                    </HStack>;
-                })}
-            </VStack>
-            : <Text color='white'>Пусто</Text>}
+                        return <HStack key={i} color='white'>
+                            <Text>{i + 1}. {PROPERTY_DISCIPLINE_NAME} {`//`} {PROPERTY_LESSON_TYPE} {`//`} {PROPERTY_LECTOR} {`//`} {PROPERTY_PLACE}</Text>
+                        </HStack>;
+                    })}
+                </VStack>
+                : <Text color='white' h='100%'>Пар нет!</Text>}
+        </motion.div>
     </VStack>
 }
