600944cb840a40e82118d788c97a5583c0bce275
cherry4xo <1jlaketuk1@gmail.com>
2023-11-14 11:59:17 +0300

    v1.3.2.0 made birthday schedule

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,13 @@
 import asyncio
 from os import environ
 from time import tzset
+from datetime import datetime
 
 from fastapi import FastAPI
 from fastapi import Request
 from fastapi.responses import HTMLResponse
+from apscheduler.schedulers.background import BackgroundScheduler
+
 from app.core.exceptions import SettingNotFound
 from app.core.init_app import (
     # configure_logging,
@@ -16,6 +19,8 @@ from app.core.init_app import (
     create_default_admin_user,
 )
 from app.redis.database import ping_redis_connection, r
+from app.schedule.tasks import check_if_birthday_and_return_name
+
 
 try:
     from app.settings.config import settings
@@ -37,6 +42,11 @@ async def db_init():
     await ping_redis_connection(r)
     register_db(app)
 
+@app.on_event("startup")
+async def schedule_birthday():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(check_if_birthday_and_return_name, 'cron', hour="9", minute="0")
+    scheduler.start()
 
 asyncio.create_task(create_default_admin_user())
 
---
+++ backend/app/schedule/tasks.py
@@ -0,0 +1,19 @@
+import json
+
+from typing import Optional
+from datetime import date
+
+from apscheduler.schedulers.background import BackgroundScheduler 
+
+
+def check_if_birthday_and_return_name() -> Optional[list]:
+    birthdays_today_names = list()
+    with open("app/settings/birthdays.json", "r") as f:
+        json_data = (json.load(f))["data"]
+    today_date = date.today()
+    for i in json_data:
+        if today_date.month == i["month"] + 1 and today_date.day == i["date"]:
+            birthdays_today_names.append(i["name"])
+    print(birthdays_today_names)
+    return birthdays_today_names
+    
