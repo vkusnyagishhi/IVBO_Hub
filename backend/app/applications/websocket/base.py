af2992e4a7264ce49260f0f3308e652e1a6ff085
cherry4xo <1jlaketuk1@gmail.com>
2023-11-18 18:57:54 +0300

    v1.3.4.0 made websocket for homework editing

---
+++ backend/app/applications/websocket/base.py
@@ -0,0 +1,42 @@
+from typing import List
+from fastapi import WebSocket
+
+
+class Singleton(object):
+    def __new__(cls, *args, **kwargs):
+        it = cls.__dict__.get("__it__")
+        if it is not None:
+            return it
+        cls.__it__ = it = object.__new__(cls)
+        it.init(*args, **kwargs)
+        return it
+    
+    def __init__(self, *args, **kwargs):
+        pass
+
+
+class ConnectionManager(Singleton):
+    def __init__(self, *args, **kwargs):
+        self.active_connections: List[WebSocket] = []
+    
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    async def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+    async def check_connection(self, websocket: WebSocket):
+        if websocket in self.active_connections:
+            return True
+        return False
+
+
+manager = ConnectionManager()
--- backend/app/applications/websocket/websocket.py
+++ backend/app/applications/websocket/websocket.py
@@ -1,106 +1,79 @@
 import asyncio
+import anyio
 from typing import List
 
-from fastapi import APIRouter, WebSocket, Depends
-from fastapi.websockets import WebSocketState
+from fastapi import APIRouter, WebSocket, Depends, Request, HTTPException
+from fastapi.websockets import WebSocketState, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
-from fastapi import HTTPException
+from fastapi.templating import Jinja2Templates
+from pydantic import UUID4
+from broadcaster import Broadcast
 
 from app.core.auth.utils.contrib import get_current_user
+from app.applications.websocket.base import ConnectionManager, manager
 from app.applications.users.models import User
+# from app.applications.homework.schemas import BaseHomeworkOut, BaseHomeworkUpdate
+from app.applications.homework.models import Homework
+from app.settings.config import settings
 
+
+broadcast = Broadcast(f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}")
+templates = Jinja2Templates("templates")
 router = APIRouter()
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:8083/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
 
+class HomeworkEditConnectionManager(ConnectionManager):
+    def __init__(self, *args, **kwargs):
+        self.active_connections: List[dict] = []
 
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: List[WebSocket] = []
-    
-    async def connect(self, websocket: WebSocket):
+    async def connect(self, 
+                      websocket: WebSocket, 
+                      homework_id: str, 
+                      current_user: User = Depends(get_current_user)
+    ):
+        if self.check_connection(websocket):
+            raise HTTPException(status_code=503, detail="Connection if busy")
         await websocket.accept()
-        self.active_connections.append(websocket)
+        self.active_connections.append({"websocket": websocket,
+                                        "homework_id": homework_id,
+                                        "user": current_user})
 
     async def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+        for items in self.active_connections:
+            if websocket == items["websocket"]:
+                del self.active_connections[items]
 
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
-
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_text(message)
-
-    async def check_connection(self, websocket: WebSocket):
-        if websocket in self.active_connections:
-            return True
+    async def check_connection(self, homework_id: str):
+        for items in self.active_connections:
+            if homework_id in items["homework_id"]:
+                return True
         return False
+    
 
-
-manager = ConnectionManager()
-
-
-# HEART_BEAT_INTERVAL = 5
-
-# async def is_websocket_active(
-#         ws: WebSocket
-# ):
-#     if not(ws.application_state == WebSocketState.CONNECTED and ws.client_state == WebSocketState.CONNECTED):
-#         return False
-#     try:
-#         await asyncio.wait_for(ws.send_json({'type': 'ping'}), HEART_BEAT_INTERVAL)
-#         message = await asyncio.wait_for(ws.receive_json(), HEART_BEAT_INTERVAL)
-#         assert message['type'] == 'pong'
-#     except BaseException:
-#         return False
-#     return True
+homework_manager = HomeworkEditConnectionManager()
 
 
 @router.get("/")
-async def get():
-    return HTMLResponse(html)
+async def homepage(request: Request):
+    template = "app/applications/websocket/index.html"
+    context = {"request": request}
+    return templates.TemplateResponse(template, context)
 
 
 @router.websocket("/ws")
 async def websocket_endpoint(
-    websocket: WebSocket
+    websocket: WebSocket,
+    homework_id: UUID4,
+    current_user: User = Depends(get_current_user),
 ):
-    if await manager.check_connection(websocket):
-        raise HTTPException(status_code=503, detail="Connetion is busy")
-    await manager.connect(websocket)
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"send: {data}")
+    await homework_manager.connect(websocket, homework_id, current_user)
+    try:
+        data = await Homework.get_or_none(uuid=homework_id)
+        if not data:
+            raise HTTPException(status_code=404, detail="The homework with this uuid does not exist")
+        while True:
+            data.text = await websocket.receive_text()
+    except WebSocketDisconnect:
+        await data.save()
+        homework_manager.disconnect(websocket)
+
