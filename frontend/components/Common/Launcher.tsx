0b492eab3a69bef526c4e868b9ad777a0324e2fd
mkrkru <42581149+mkrkru@users.noreply.github.com>
2023-11-20 13:11:49 +0300

    upd

--- frontend/components/Common/Launcher.tsx
+++ frontend/components/Common/Launcher.tsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from '@/redux/hooks';
 import { setAuthData, setUserpic } from "@/redux/authSlice";
 import axios from "axios";
 import { Text } from "@chakra-ui/react";
+import { api } from "@/utils/api";
 
 export function Launcher() {
     const dispatch = useDispatch();
@@ -27,16 +28,26 @@ export function Launcher() {
         // if (!localStorage.getItem('weeksDisplayCount')) localStorage.setItem('weeksDisplayCount', '4');
         // else dispatch(setWeeksDisplayCount(parseInt(localStorage.getItem('weeksDisplayCount') ?? '4')));
 
-        if (!user && localStorage.getItem('ivbo_token')) axios.post('https://api.twodev.cc/ivbo/login', { hash: localStorage.getItem('ivbo_token') }).then(res => {
-            if (res.data === 500) {
-                localStorage.removeItem('ivbo_token');
-                dispatch({ type: 'socket/connect', payload: 'unknown' });
-            } else {
-                dispatch(setAuthData(res.data));
-                dispatch({ type: 'socket/connect', payload: res.data.user.tg_id.toString() });
-            }
-        });
-        else dispatch({ type: 'socket/connect', payload: 'unknown' });
+        if (!user && localStorage.getItem('ivbo_auth')) {
+            const token = localStorage.getItem('ivbo_auth');
+            if (!token) return localStorage.removeItem('ivbo_auth');
+
+            api.get('/auth/users/me', { headers: { Authorization: localStorage.getItem('ivbo_auth') } })
+                .then(res => {
+                    console.log(res.data);
+                });
+        }
+
+        // if (!user && localStorage.getItem('ivbo_token')) axios.post('https://api.twodev.cc/ivbo/login', { hash: localStorage.getItem('ivbo_token') }).then(res => {
+        //     if (res.data === 500) {
+        //         localStorage.removeItem('ivbo_token');
+        //         dispatch({ type: 'socket/connect', payload: 'unknown' });
+        //     } else {
+        //         dispatch(setAuthData(res.data));
+        //         dispatch({ type: 'socket/connect', payload: res.data.user.tg_id.toString() });
+        //     }
+        // });
+        // else dispatch({ type: 'socket/connect', payload: 'unknown' });
     }, [dispatch, user, version]);
 
     return <>
--- frontend/components/Common/TgLoginButton.tsx
+++ frontend/components/Common/TgLoginButton.tsx
@@ -13,33 +13,35 @@ export function TgLoginButton() {
     const router = useRouter();
 
     return <TelegramLoginButton
-        botName={process.env.NODE_ENV === 'development' ? 'twodev_helper_bot' : 'ivbo1123bot'}
+        botName={process.env.NODE_ENV === 'development' ? 'cherry4xo_validation_test_bot' : 'ivbo1123bot'}
         cornerRadius={20}
         usePic={true}
         // dataAuthUrl='http://127.0.0.1/test'
         dataOnauth={(data: TelegramUser) => {
-            if (data.id === 0) return;
+            if (data.id === 0) return router.push('/');
             localStorage.setItem('tg_userpic', data.photo_url);
 
-            // api.post('/auth/login/telegram/generate-token', data)
-            //     .then((res: any) => api.post('/auth/login/access-token', `username=${encodeURIComponent(res.data.username)}&password=${encodeURIComponent(res.data.short_token)}`, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
-            //         .then(res => {
-            //             localStorage.setItem('ivbo_token', res.data.access_token);
-            //             router.refresh();
-            //         })
-            //         .catch(res => console.log(res.response.data.detail)))
-            //     .catch(err => {
-            //         if (!toast.isActive('error-toast')) toast(toasts.error(err.response.data.detail[0].msg));
-            //     });
+            api.post('/auth/login/telegram/generate-token', data)
+                .then((res: any) => {
+                    api.post('/auth/login/access-token', `username=${encodeURIComponent(res.data.username)}&password=${encodeURIComponent(res.data.short_token)}`, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
+                        .then(res => {
+                            localStorage.setItem('ivbo_auth', `${res.data.token_type} ${res.data.access_token}`);
+                            router.refresh();
+                        })
+                        .catch(err => console.error(err));
+                })
+                .catch(err => {
+                    if (!toast.isActive('error-toast')) toast(toasts.error(err.response.data.detail[0].msg));
+                });
 
-            axios.post('https://api.twodev.cc/ivbo/login', data).then(res => {
-                if (res.data === 500) {
-                    if (!toast.isActive('error-toast')) toast(toasts.error('Вы не состоите в группе ИВБО-11-23!'));
-                } else {
-                    dispatch(setAuthData(res.data));
-                    localStorage.setItem('ivbo_token', data.hash);
-                }
-            });
+            // axios.post('https://api.twodev.cc/ivbo/login', data).then(res => {
+            //     if (res.data === 500) {
+            //         if (!toast.isActive('error-toast')) toast(toasts.error('Вы не состоите в группе ИВБО-11-23!'));
+            //     } else {
+            //         dispatch(setAuthData(res.data));
+            //         localStorage.setItem('ivbo_token', data.hash);
+            //     }
+            // });
         }}
     />
 }
