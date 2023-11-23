460b07800b97ac3caf2f6b3b6d44be6da3553109
cherry4xo <1jlaketuk1@gmail.com>
2023-11-23 10:59:49 +0300

    v1.3.5.3 sample for tasks

---
+++ backend/app/applications/additional/routers.py
@@ -0,0 +1,6 @@
+from fastapi import APIRouter
+
+from app.applications.additional.schemas import * # put your schemas instead of * sign
+
+
+# You should put your routers into this file
---
+++ backend/app/applications/additional/schemas.py
@@ -0,0 +1,5 @@
+from typing import Optional, List
+
+from pydantic import BaseModel
+
+# You should put your schemas into this file
